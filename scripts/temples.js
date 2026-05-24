1fe7e9b013a521f41e12f076c928237e168551f8
fredywnavas <fnavasmartinez@byupathway.edu>
2026-05-23 20:42:44 -0700

    updating from vscode

---
+++ scripts/temples.js
@@ -0,0 +1,12 @@
+const menuButton = document.querySelector("#menu-button");
+const nav = document.querySelector("nav");
+
+menuButton.addEventListener("click", () => {
+    nav.classList.toggle("open");
+
+    if (nav.classList.contains("open")) {
+        menuButton.textContent = "X";
+    } else {
+        menuButton.textContent = "☰";
+    }
+});
