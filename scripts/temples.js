1ebd23bb54ed01b8531f9c5864866f5591ac4adc
fredywnavas <fnavasmartinez@byupathway.edu>
2026-05-23 20:11:57 -0700

    changed code from temples.js to use the getdates.js file instead

--- scripts/temples.js
+++
@@ -1,4 +0,0 @@
-const year = new Date().getFullYear();
-document.getElementById("currentyear").textContent = year;
-
-document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
